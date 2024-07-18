00b7f575bfdaf9b90a7bdf8a2f37e1f6d838979d
Maria <145348994+MariaBezdudnaya@users.noreply.github.com>
2024-07-18 20:06:02 +0500

    Add tasks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
-//1. Использование операторов и условий:
+// Создайте массив initData, содержащий числа от 1 до 100. Создайте пустой массив results.
+// 1. Использование операторов и условий. Используйте любой цикл для обхода массива initData. При каждой итерации цикла, используйте операторы для увеличения текущего числа на два. Если полученное число четное, добавьте его в массив results.
 let initData = Array.from({ length: 100 }, (_, index) => index + 1);
 let results = [];
 for (let i = 0; i < initData.length; i++) {
@@ -11,7 +12,7 @@ for (let i = 0; i < initData.length; i++) {
 console.log('1. Использование операторов и условий:');
 console.log(initData);
 console.log(results);
-//2. Создание нового массива:
+// 2. Создание нового массива. Создайте пустой массив squareRootResults. Снова используйте цикл для обхода массива results. На каждой итерации цикла, вычислите квадратный корень текущего числа, и добавьте результат в squareRootResults.
 let squareRootResults = [];
 for (let i = 0; i < results.length; i++) {
     let sqrt = Math.sqrt(results[i]);
@@ -19,7 +20,7 @@ for (let i = 0; i < results.length; i++) {
 }
 console.log('2. Создание нового массива:');
 console.log(squareRootResults);
-//3. Условия и вывод данных. Создайте условие, которое проверяет, есть ли в squareRootResults хотя бы одно число, большее 7. Если такое число есть, выведите его в консоль
+// 3. Условия и вывод данных. Создайте условие, которое проверяет, есть ли в squareRootResults хотя бы одно число, большее 7. Если такое число есть, выведите его в консоль.
 let hasNumber = squareRootResults.some(num => num > 7);
 console.log('3. Условия и вывод данных:');
 console.log(hasNumber);
